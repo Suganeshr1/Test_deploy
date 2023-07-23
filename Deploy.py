7aa9313905620b4321d4ad5a18ce886798f5d4f2
Suganesh <121660395+Suganeshr1@users.noreply.github.com>
2023-07-23 20:17:43 +0530

    Update Deploy.py

--- Deploy.py
+++ Deploy.py
@@ -7,7 +7,7 @@
 import numpy as np
 import pandas as pd
 # import seaborn as sns
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 from warnings import filterwarnings
 filterwarnings("ignore")
